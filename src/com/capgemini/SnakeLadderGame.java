9bad9d66b403254b70c975dd38edd42a9ddb2b4e
krove <krove@DESKTOP-Q5SED1R>
2020-09-26 10:42:39 +0530

    UC7 added. Gameplay with 2 players and reporting which player won the game

--- src/com/capgemini/SnakeLadderGame.java
+++ src/com/capgemini/SnakeLadderGame.java
@@ -4,28 +4,62 @@ public class SnakeLadderGame {
 	public static int noPlay =0,ladder = 1,snake =2;
 	public static void main(String[] args) {
 		System.out.println("Welcome to the game");
-		int pos = 0, no_of_turns =0;
-		while(pos < 100) {
-		int dice_value = (int) Math.floor(Math.random()*10)%6 +1;
-		no_of_turns ++;
-		System.out.println("Number on the dice: "+dice_value);
-		int opt = (int) Math.floor(Math.random()*10)%3;
-		if(opt == noPlay) {
-			System.out.println("Player remains at the same position");
+		int pos1 = 0,pos2 =0, p1_turns =0,p2_turns = 0;
+		boolean is_p1_turn = true,is_p2_turn =false;
+		while((pos1 < 100)&&(pos2 <100)) {
+			int dice_value = (int) Math.floor(Math.random()*10)%6 +1;
+			int opt = (int) Math.floor(Math.random()*10)%3;
+			if(is_p1_turn) {
+				p1_turns++;
+				if(opt == noPlay) {
+					System.out.println("Player1 remains at the same position");
+					is_p1_turn = false;
+					is_p2_turn = true;
+				}
+				else if(opt == ladder) {
+					if(pos1+dice_value <=100)
+						pos1 += dice_value;
+				}
+				else if(opt == snake) {
+					if(pos1-dice_value > 0)
+					 pos1 -= dice_value;
+					else
+						pos1 = 0;
+					is_p1_turn = false;
+					is_p2_turn = true;
+				}
+				System.out.println("Player1's position: "+pos1);
+			}
+			else if(is_p2_turn){
+				p2_turns++;
+				if(opt == noPlay) {
+					System.out.println("Player2 remains at the same position");
+					is_p1_turn = true;
+					is_p2_turn = false;
+				}
+				else if(opt == ladder) {
+					if(pos2+dice_value <=100)
+						pos2 += dice_value;
+				}
+				else if(opt == snake) {
+					if(pos2-dice_value > 0)
+					 pos2 -= dice_value;
+					else
+						pos2 = 0;
+					is_p1_turn = true;
+					is_p2_turn = false;
+				}
+				System.out.println("Player2's position: "+pos2);
+			}
 		}
-		else if(opt == ladder) {
-			if(pos+dice_value <=100)
-				pos += dice_value;
+		System.out.println("Player1's final position:"+pos1);
+		System.out.println("Player2's final position:"+pos2);
+		
+		if(pos1 == 100) {
+			System.out.println("Player1 won the game and he rolled the dice :"+p1_turns+" times");
 		}
-		else if(opt == snake) {
-			if(pos-dice_value > 0)
-			 pos -= dice_value;
-			else
-				pos = 0;
+		else {
+			System.out.println("Player2 won the game and he rolled the dice :"+p2_turns+" times");
 		}
-		System.out.println("Position of player: "+pos);
 		}
-		System.out.println("Player's final position: "+pos);
-		System.out.println("Number of times the dice rolled is: "+no_of_turns);
-	}	
 }
