1ea51189ede395797c8ffbea08f643d52c92daee
krove <krove@DESKTOP-Q5SED1R>
2020-09-26 10:36:01 +0530

    UC3 added. Checking for options- NoPlay,ladder and snake using random()

--- src/com/capgemini/SnakeLadderGame.java
+++ src/com/capgemini/SnakeLadderGame.java
@@ -1,10 +1,21 @@
 package com.capgemini;
 
 public class SnakeLadderGame {
+	public static int noPlay =0,ladder = 1,snake =2;
 	public static void main(String[] args) {
 		System.out.println("Welcome to the game");
 		int pos = 0;
 		int dice_value = (int) Math.floor(Math.random()*10)%6 +1;
 		System.out.println("Number on the dice: "+dice_value);
+		int opt = (int) Math.floor(Math.random()*10)%3;
+		if(opt == noPlay) {
+			System.out.println("Player remains at the same position");
+		}
+		else if(opt == ladder) {
+			pos += dice_value;
+		}
+		else if(opt == snake) {
+			pos -= dice_value;
+		}
 	}
 }
